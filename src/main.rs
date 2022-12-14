4bd11966a9d77287e732ca9b16ed56275b00d4c7
MorganB <morganbrockman1998@gmail.com>
2022-12-14 01:24:33 -0800

    rewrote to be simpler; pulls from Random.org now

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,49 @@
-use std::io::stdin;
+use std::io;
 use rand::Rng;
+use ureq::get;
 
 fn main() {
-    let mut list_list: Vec<Vec<String>> = Vec::new();
-    let mut is_looping = true;
-    while is_looping {
+    let list = populate_list();
+    let list = weight_list(list);
+    println!("{}", random_item_from_list(list));
+}
 
-        let mut list: Vec<String> = Vec::new();
-        println!("Enter List:");
-        loop {
-            let mut title = String::new();
-            stdin().read_line(&mut title).expect("invalid title");
-            match title.trim() {
-                "add" => {
-                    list.truncate(10);
-                    list_list.push(list);
-                    println!();
-                    break;
-                },
-                "end" => {
-                    list.truncate(10);
-                    list_list.push(list);
-                    is_looping = false;
-                    println!();
-                    break;
-                },
-                _ => {
-                    list.push(title.to_string());
-                }
-            }
-        }
+fn populate_list() -> Vec<String> {
+    let mut list = Vec::new();
+    let mut buffer = String::new();
+    loop {
+        io::stdin().read_line(&mut buffer).expect("input should be readable");
+        if buffer.trim().len() == 0 { break; }
+        list.push(buffer.trim().to_owned());
+        buffer.clear();
     }
+    list
+}
+
+fn weight_list(list: Vec<String>) -> Vec<String> {
     let mut weighted_list: Vec<String> = Vec::new();
-    for list in list_list {
-        let mut item_count = list.len();
-        for item in list {
-            let mut weight = item_count;
-            while weight > 0 {
-                weighted_list.push(item.clone());
-                weight -= 1;
-            }
-            item_count -= 1;
+    for (i, item) in list.iter().enumerate() {
+        for _ in 0..(list.len()-i) {
+            weighted_list.push(item.to_owned())
         }
     }
-    println!("Result:");
-    println!("{}", weighted_list[rand::thread_rng().gen_range(1..weighted_list.len())]);
+    weighted_list
+}
+
+fn random_item_from_list(list: Vec<String>) -> String {
+    let result;
+    let mut url = "https://www.random.org/integers/?num=1&min=0&col=1&base=10&format=plain&rnd=new&max=".to_string();
+    url.push_str(&list.len().to_string()[..]);
+    match get(&url).call() {
+        Ok(resp) => {
+            // println!("{}", resp.into_string().unwrap());
+            result = resp.into_string().unwrap().trim().parse().unwrap();
+            // result = 0;
+        }
+        Err(_) => {
+            println!("Random.org request failed. Generating result locally...");
+            result = rand::thread_rng().gen_range(0..list.len());
+        }
+    }
+    list[result].to_owned()
 }
